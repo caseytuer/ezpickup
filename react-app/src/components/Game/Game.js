807d69f376595f4221ba62e1505d0910bc9d103b
Casey Tuer <caseytuer@gmail.com>
2021-08-27 11:43:48 -0700

    game page initial styling complete

--- react-app/src/components/Game/Game.js
+++ react-app/src/components/Game/Game.js
@@ -3,8 +3,12 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useHistory, useParams } from 'react-router-dom';
 import { deleteGame, getGame } from '../../store/game';
 import { updateGame } from '../../store/game';
+import { Link } from 'react-router-dom';
 import './Game.css'
 import bannerImage from '../../assets/images/bball-sillouette.png';
+import Map from '../Map';
+import UsersList from '../UsersList';
+import User from '../User';
 
 
 
@@ -14,9 +18,19 @@ const Game = () => {
     const dispatch = useDispatch();
     let { gameId } = useParams();
     const game = useSelector(state => state.game[gameId])
-    console.log(game)
 
+    const [users, setUsers] = useState([]);
     const [title, setTitle] = useState();
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch('/api/users/');
+            const responseData = await response.json();
+            setUsers(responseData.users);
+        }
+        fetchData();
+    }, []);
+
     // const [sport, setSport] = useState(game?.sport);
     // const [description, setDescription] = useState(game?.description);
     // const [equipmentNeeded, setEquipmentNeeded] = useState(game?.equipment_needed);
@@ -64,9 +78,8 @@ const Game = () => {
             country: game.country,
             lat: game.lat,
             lng: game.lng,
-            start_time: '2021-12-16 12:00:00',
-            end_time: '2021-12-16 16:00:00',
-            created_at: '2021-12-16 12:00:00'
+            start_time: game.start_time,
+            end_time: game.end_time,
         }
         console.log(payload)
         dispatch(updateGame(payload))
@@ -84,56 +97,89 @@ const Game = () => {
         }
     }
 
+    const findCreator = (game) => {
+        const user = users?.find(user => user?.id === game?.creator_id)
+        return user?.username
+    }
+
+    const skillLevel = ['none', 'Beginner', 'Intermediate', 'Advanced']
+
+    const handleTime = (time) => {
+        const militaryTime = time?.split(' ')[4]
+        let hours = militaryTime?.split(':')[0]
+        if (Number(hours) < 10) {
+            hours = hours?.split('')[1]
+            return `${hours}:${militaryTime?.split(':').slice(1, 2).join(':')}AM `
+        } else if (Number(hours) < 12) {
+            return `${militaryTime?.split(':').slice(0,2).join(':')}AM `
+        } else if (Number(hours) === 12) {
+            return `${militaryTime?.split(':').slice(0,2).join(':')}PM `
+        } else {
+            return `${hours % 12}:${militaryTime?.split(':').slice(1,2).join(':')}PM `
+        }
+    }
+
+
     return (
-        <div className="game-page-canvas">
-            <div className="game-page-side-banner">
-                <img src={bannerImage} alt=""></img>
+        <>
+            <div>
+                <Map game={game} />
             </div>
-            <div className="game-page-container">
-                {/* <button onClick={handleDelete}>delete game</button> */}
-                <form onSubmit={handleEdit} className="game-card-field" id="game-card-title-edit-btn" >
-                    <div>
-                        {/* <button type='submit'>edit</button> */}
-                        <div 
-                        className="game-page-title" id="game-card-title-field" >{game?.title} </div>
-                        {/* <div>
-                            <input
-                                placeholder={game?.title}
-                                onChange={(e) => setTitle(e.target.value)}
-                                type="text"
-                            />
-                            <button onClick={handleSubmitTitle}type='submit'>save</button>
-                        </div> */}
+            <div className="find-another-game">
+                <Link to="/" className="find-another-game-btn">Find Another Game</Link>
+            </div>
+            <div className="game-page-canvas">
+                <div className="game-page-side-banner">
+                    <img src={bannerImage} alt=""></img>
+                </div>
+                <div className="game-page-container">
+                    {/* <button onClick={handleDelete}>delete game</button> */}
+                    <form onSubmit={handleEdit} className="game-card-field" id="game-card-title-edit-btn" >
+                        <div>
+                            {/* <button type='submit'>edit</button> */}
+                            <div 
+                            className="game-page-title" id="game-card-title-field" >{game?.title} 
+                                <span className="game-card-field game-page-sport">{` (${game?.sport})`} 
+                                </span>
+                            </div>
+                            {/* <div>
+                                <input
+                                    placeholder={game?.title}
+                                    onChange={(e) => setTitle(e.target.value)}
+                                    type="text"
+                                />
+                                <button onClick={handleSubmitTitle}type='submit'>save</button>
+                            </div> */}
+                        </div>
+                        <div>
+                            <div></div>
+                        </div>
+                    </form>
+                        <div className="game-card-field" >
+                            <span className="game-page-details">
+                                {`${game?.address}, `} 
+                            </span>
+                            <span className="game-page-details">
+                                {`${game?.city}, `}
+                            </span>
+                            <span className="game-page-details">
+                                {game?.state}
+                            </span>
+                        </div>
+                        <div className="game-card-field game-page-details" >
+                            <span>{game?.start_time.split(' ').slice(0,4).join(' ')}</span>
+                        </div>
+                    <div className="game-page-where-when"></div>
+                    <div className="game-card-field" >
+                        <span className="game-page-details"></span>
+                        <span className="game-page-details">{handleTime(game?.start_time)}- </span>
+                        <span className="game-page-details">{handleTime(game?.end_time)}</span>
                     </div>
-                    <div>
-                        <div></div>
-                    </div>
-                </form>
-                <div className="game-card-field">
-                    <div className="game-page-sport">{game?.sport} </div>
-                </div>
-                <div className="game-card-field" >{game?.description}</div>
-                <div className="game-card-field" >
-                    <span>
-                        {`@ ${game?.address}, `} 
-                    </span>
-                    <span>
-                        {`${game?.city}, `}
-                    </span>
-                    <span>
-                        {game?.state}
-                    </span>
-                </div>
-                <div className="game-card-field" >
-                    <span>starts at: </span>
-                    <span>{game?.start_time}</span>
-                </div>
-                <div className="game-card-field" >
-                    <span>ends at: </span>
-                    <span>{game?.end_time}</span>
+                <div className="game-card-field game-page-description">{`"${game?.description}"`}</div>
+                <div>{`-${findCreator(game)}`}</div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
