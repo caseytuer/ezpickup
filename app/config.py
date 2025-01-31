7049e6bfb3a992fdd607f67855f2980478bcd53d
Casey Tuer <61769975+caseytuer@users.noreply.github.com>
2025-01-30 22:03:19 -0800

    Update config.py

--- app/config.py
+++ app/config.py
@@ -5,5 +5,5 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL').replace('postgres://', 'postgresql://')
+        'DATABASE_URL')
     SQLALCHEMY_ECHO = True
